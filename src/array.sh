9204adbc3152cb1c4a1081188e68f4e6e7a7824b
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-08-11 20:11:07 +0900

    feat(src): add array examples

---
+++ src/array.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+array=(123 "foo bar" baz)
+echo "array[0]: ${array[0]}"
+echo "array[1]: ${array[1]}"
+echo "array[2]: ${array[2]}"
+echo "array[3]: ${array[3]}"
+echo "length: ${#array[*]}"
+
+index=1
+echo "array[index=$index]: ${array[$index]}"
